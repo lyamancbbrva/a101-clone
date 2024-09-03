151698db7ddd2714cf4975982159a527d796abfa
Laman Cabbarova <lyamancbbrva@gmail.com>
2024-09-03 22:32:39 +0400

    header

---
+++ src/components/header/Category.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Category() {
+  return (
+    <div>
+        <ul className='flex gap-7 justify-center border-b border-t py-2'>
+            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+        </ul>
+    </div>
+  )
+}
+
+export default Category
