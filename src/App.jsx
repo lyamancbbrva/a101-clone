4c8d83b9c5fc6211e889e3ec26e37efd80ca622c
Laman Cabbarova <lyamancbbrva@gmail.com>
2024-09-05 19:24:02 +0400

    jhgfdfghj

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { Route, Routes, useLocation } from "react-router-dom"
 import Main from "./components/main/Main"
 import Layout from "./layout/Layout"
 import Basket from "./components/main/Basket"
-<<<<<<< HEAD
 import Error404 from "./components/Error404"
-=======
 import Kampanyalar from "./components/main/Kampanyalar"
 import { useEffect } from "react"
->>>>>>> 271724f0b475a092069b6999593c166df731c101
 
 function App() {
     const { pathname } = useLocation()
@@ -21,12 +18,9 @@ function App() {
                 <Route path='/' element={<Layout />}>
                     <Route path='/' element={<Main />} />
                     <Route path='/basket' element={<Basket />} />
-<<<<<<< HEAD
                     <Route path='*' element={<Error404/>} />
-=======
                     <Route path='/kampanyalar' element={<Kampanyalar />} />
                     <Route path='*' element={<Error404 />} />
->>>>>>> 271724f0b475a092069b6999593c166df731c101
                 </Route>
             </Routes>
         </>
--- src/components/header/Category.jsx
+++ src/components/header/Category.jsx
@@ -4,15 +4,15 @@ function Category() {
   return (
     <div>
         <ul className='hidden lg:flex gap-7 justify-center border-b border-t py-2'>
-            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
-            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
-            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
-            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
-            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
-            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
-            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
-            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
-            <a href=""><li className='border-l border-r px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-r px-2 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-r px-2 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-r px-2 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-r px-2 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-r px-2 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-r px-2 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-r px-2 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='         px-2 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
+            <a href=""><li className='border-l px-4 font-medium leading-6 text-[.625em]'>Elektronik</li></a>
         </ul>
     </div>
   )
